5c78e5a5b78b93708394af0412f6567033dff9ae
DESKTOP-B4F4HKA\user <michal.rusek.11@gmail.com>
2023-01-31 19:14:39 +0100

    Utworzenie funkcji dodawania

--- TestGH/TestGH/Program.cs
+++ TestGH/TestGH/Program.cs
@@ -1,3 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("witaj świecie ");
+
+int Dodaj(int a, int b )
+{
+    return a + b;
+
+}
