17c63b713f5c8f8b540f22e254649fbfe1f1b524
DESKTOP-B4F4HKA\user <michal.rusek.11@gmail.com>
2023-01-31 19:17:13 +0100

    dodano funkcję odejmowania

--- TestGH/TestGH/Program.cs
+++ TestGH/TestGH/Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("witaj świecie ");
+
+int Odejmij(int a, int b)
+{
+    return a - b;
+}
